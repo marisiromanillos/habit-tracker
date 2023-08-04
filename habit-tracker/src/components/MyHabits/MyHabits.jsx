dd053d6fb739c9be62d7b4aa4d121f7452ad3525
Marisi Romanillos <marisiromanillos@Marisis-MBP.lan>
2023-08-04 12:46:12 +0100

    changed font

--- habit-tracker/src/components/MyHabits/MyHabits.jsx
+++ habit-tracker/src/components/MyHabits/MyHabits.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+
+// Defines Desktop and Mobile Containers
+const DesktopContainer = styled.div`
+  @media only screen and (max-width: 699px) {
+    display: none;
+  }
+`;
+
+const MobileContainer = styled.div`
+  @media only screen and (min-width: 700px) {
+    display: none;
+  }
+`;
 
 const MyHabits = () => {
   return (
     <>
-      <div>MyHabits</div>
+      <DesktopContainer>{/* <MyHabitsDesktop /> */}</DesktopContainer>
+      <MobileContainer>Mobile</MobileContainer>
     </>
   );
 };
